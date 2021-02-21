875449b1d2f388d77534e723569eacbce95d891c
George <naifonovgk@gmail.com>
2021-02-21 22:11:16 +0300

    Fix disableFunction fixed

--- js/state.js
+++ js/state.js
@@ -4,19 +4,22 @@ const mapForm = document.querySelector('.map__filters');
 const mapFormItems = mapForm.querySelectorAll('.map__filter')
 const mapFeatures = mapForm.querySelector('.map__features');
 
+
+const disableElements = (arr) => {
+  arr.forEach((item) => {
+    item.disabled = true;
+  })
+}
+
 const disableForms = () => {
   mainForm.classList.add('ad-form--disabled');
   mapForm.classList.add('map__filters--disabled');
-  mapFeatures.setAttribute('disabled', true);
+  mapFeatures.disabled = true;
 
-  mainFormItems.forEach((item) => {
-    item.setAttribute('disabled', true);
-  })
-
-  mapFormItems.forEach((item) => {
-    item.setAttribute('disabled', true);
-  })
+  disableElements(mainFormItems);
+  disableElements(mapFormItems);
 }
+
 
 export {disableForms};
 
