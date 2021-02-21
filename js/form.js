d89da2210a70ec06a8b04d9ea4d783d1987d6f99
George <naifonovgk@gmail.com>
2021-02-21 12:35:39 +0300

    Feat propety to price sync function added

--- js/form.js
+++ js/form.js
@@ -1,6 +1,8 @@
 const mainForm = document.querySelector('.ad-form');
 const timeIn = mainForm.querySelector('#timein');
 const timeOut = mainForm.querySelector('#timeout');
+const priceInput = mainForm.querySelector('#price');
+const propertyType = mainForm.querySelector('#type');
 
 
 const syncSelect = (evt) => {
@@ -13,6 +15,27 @@ const syncSelect = (evt) => {
   }
 }
 
+const priceToType = {
+  'bungalow': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000,
+}
 
-timeIn.addEventListener('change', syncSelect);
-timeOut.addEventListener('change', syncSelect);
+const syncPriceToType = () => {
+  priceInput.min = priceToType[propertyType.value];
+  priceInput.placeholder = priceToType[propertyType.value];
+}
+
+
+const validateForm = () => {
+  syncPriceToType();
+  propertyType.addEventListener('change', syncPriceToType);
+  priceInput.addEventListener('input', syncPriceToType);
+  timeIn.addEventListener('change', syncSelect);
+  timeOut.addEventListener('change', syncSelect);
+}
+
+
+export {validateForm};
+
--- js/main.js
+++ js/main.js
@@ -2,7 +2,12 @@ import './data.js';
 import './popup.js'
 import {createCard, similarOffers} from './popup.js';
 import './form.js';
+import {validateForm} from './form.js';
+
 
 const map = document.querySelector('#map-canvas');
 
+
 map.appendChild(createCard(similarOffers[0]));
+
+validateForm();
