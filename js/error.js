d02637fdb91f591e4eb4aab925e2cb66cb74981b
George <naifonovgk@gmail.com>
2021-02-26 21:30:39 +0300

    Feat get error message function added

--- js/error.js
+++ js/error.js
@@ -6,10 +6,32 @@ const errorTemplate = document.querySelector('#error')
 const documentFragment = document.createDocumentFragment();
 const errorMessage = errorTemplate.cloneNode(true);
 
-const showErrorMessage = () => {
+const showSendErrorMessage = () => {
   mapContainer.style.zIndex = 0;
   documentFragment.appendChild(errorMessage);
   document.body.appendChild(documentFragment);
 }
 
-export {showErrorMessage}
+
+
+const showGetErrorMessage = () => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = 'Ошибка получения данных';
+
+  document.body.append(alertContainer);
+}
+
+export {
+  showSendErrorMessage,
+  showGetErrorMessage
+}
--- js/server.js
+++ js/server.js
@@ -1,7 +1,11 @@
 import {renderMarkers} from './map.js';
+import {showGetErrorMessage} from './error.js';
 
 fetch('https://22.javascript.pages.academy/keksobooking/data')
   .then((response) => response.json())
   .then((data) => {
     renderMarkers(data);
+  })
+  .catch(() => {
+    showGetErrorMessage();
   })
