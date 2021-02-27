87bd9ab49324d64cb633da51959ef9e22eefe9f6
George <naifonovgk@gmail.com>
2021-02-27 20:00:30 +0300

    Feat title validation finished

--- js/form.js
+++ js/form.js
@@ -3,6 +3,7 @@ import {sendData} from './server.js';
 import {fillAddressInput} from './map.js';
 import {showSendSuccessMessage} from './success.js';
 import {resetMarker} from './map.js';
+import {declOfNum} from './util.js';
 
 const mainForm = document.querySelector('.ad-form');
 const timeIn = mainForm.querySelector('#timein');
@@ -11,6 +12,12 @@ const priceInput = mainForm.querySelector('#price');
 const propertyType = mainForm.querySelector('#type');
 const addressInput = mainForm.querySelector('#address');
 const resetFormButton = mainForm.querySelector('.ad-form__reset')
+const inputTitle = mainForm.querySelector('#title');
+
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+const SYMBOL_WORDS = ['символ', 'символа', 'символов']
 
 const priceToType = {
   bungalow: 0,
@@ -41,7 +48,7 @@ const validateForm = () => {
 addressInput.readOnly = true;
 
 const resetForm = () => {
-  // mainForm.reset = true;
+  mainForm.reset = true;
   //todo не работает
   fillAddressInput();
 }
@@ -64,6 +71,37 @@ mainForm.addEventListener('submit', (evt) => {
 
   sendData(successHandler, showSendErrorMessage, formData)
 });
+
+
+//Я так понимаю, эти штуки конфликтуют друг с другом. Какую оставлять?
+inputTitle.addEventListener('invalid', () => {
+  if (inputTitle.validity.tooShort) {
+    //todo получить значения из разметки
+    inputTitle.setCustomValidity(`Минимальная длина ${MIN_TITLE_LENGTH} ${declOfNum(MAX_TITLE_LENGTH, SYMBOL_WORDS)}.`);
+  } else if(inputTitle.validity.tooLong) {
+    inputTitle.setCustomValidity(`Максимальная длина ${MAX_TITLE_LENGTH} ${declOfNum(MIN_TITLE_LENGTH, SYMBOL_WORDS)}.`);
+  } else if (inputTitle.validity.valueMissing) {
+    inputTitle.setCustomValidity('Это обязательное поле!');
+  } else {
+    inputTitle.setCustomValidity('');
+  }
+});
+
+inputTitle.addEventListener('input', () => {
+  const valueLength = inputTitle.value.length;
+  const LESS_SYMBOLS = MIN_TITLE_LENGTH - valueLength;
+  const MORE_SYMBOLS = valueLength - MAX_TITLE_LENGTH;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    inputTitle.setCustomValidity(`Ещё ${LESS_SYMBOLS} ${declOfNum(LESS_SYMBOLS, SYMBOL_WORDS)}`);
+  } else if(valueLength > MAX_TITLE_LENGTH) {
+    inputTitle.setCustomValidity(`Удалите ${MORE_SYMBOLS} ${declOfNum(MORE_SYMBOLS, SYMBOL_WORDS)}`);
+  } else {
+    inputTitle.setCustomValidity('');
+  }
+
+  inputTitle.reportValidity();
+})
 
 
 export {validateForm};
