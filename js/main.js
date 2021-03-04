a38811719e1dbdea019df060829e5d5b5eb844de
George <naifonovgk@gmail.com>
2021-03-04 20:21:40 +0300

    Feat invalid form if map not loaded function added

--- js/main.js
+++ js/main.js
@@ -6,13 +6,19 @@ import {getData} from './server.js';
 import {renderMarkers} from './map.js';
 import {showGetErrorMessage} from './error.js';
 import {validateForm} from './form.js';
+import {disableForms} from './state.js';
 
 const CARDS_COUNT = 10;
 
 validateForm();
 
+const showOnFail = () => {
+  showGetErrorMessage();
+  disableForms();
+}
+
 getData((data) => {
   renderMarkers(data.slice(0, CARDS_COUNT))
-}, showGetErrorMessage);
+}, showOnFail);
 
 
