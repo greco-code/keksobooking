812fc5ff6126bb711e68af293a6f647fefeaf10a
George <naifonovgk@gmail.com>
2021-03-08 17:21:31 +0300

    Feat rooms filter finished

--- js/filter.js
+++ js/filter.js
@@ -3,19 +3,20 @@ import {cleanMarkers, renderMarkers} from './map.js';
 const filterForm = document.querySelector('.map__filters');
 const filterType = filterForm.querySelector('#housing-type');
 // const filterPrice = filterForm.querySelector('#housing-price');
-// const filterRooms = filterForm.querySelector('#housing-rooms');
+const filterRooms = filterForm.querySelector('#housing-rooms');
 // const filterGuests = filterForm.querySelector('#housing-guests');
 // const filterFeatures = filterForm.querySelector('#housing-features');
 const NOT_SELECTED = 'any'
 const CARDS_COUNT = 10;
 
-
 const filterByType = (card) => card.offer.type === filterType.value || filterType.value === NOT_SELECTED;
+const filterByRooms = (card) => card.offer.rooms === Number.parseInt(filterRooms.value, 10) || filterRooms.value === NOT_SELECTED;
+
 
 const filterMarkers = (cards) => {
   return cards
-    .filter(card => filterByType(card))
-    .slice(0, CARDS_COUNT)
+    .filter(card => filterByType(card) && filterByRooms(card))
+    .slice(0, CARDS_COUNT);
 }
 
 const updateMarkers = (data) => {
@@ -30,4 +31,5 @@ const setFilterListener = (data) => {
   })
 }
 
-export {setFilterListener, filterByType}
+
+export {setFilterListener}
