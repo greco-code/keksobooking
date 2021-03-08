cdc10865def078578d11e25740b7c9025f645634
George <naifonovgk@gmail.com>
2021-03-08 20:27:04 +0300

    Feat filter by guests function finished

--- js/filter.js
+++ js/filter.js
@@ -4,18 +4,19 @@ const filterForm = document.querySelector('.map__filters');
 const filterType = filterForm.querySelector('#housing-type');
 // const filterPrice = filterForm.querySelector('#housing-price');
 const filterRooms = filterForm.querySelector('#housing-rooms');
-// const filterGuests = filterForm.querySelector('#housing-guests');
+const filterGuests = filterForm.querySelector('#housing-guests');
 // const filterFeatures = filterForm.querySelector('#housing-features');
 const NOT_SELECTED = 'any'
 const CARDS_COUNT = 10;
 
 const filterByType = (card) => card.offer.type === filterType.value || filterType.value === NOT_SELECTED;
 const filterByRooms = (card) => card.offer.rooms === Number.parseInt(filterRooms.value, 10) || filterRooms.value === NOT_SELECTED;
+const filterByGuests = (card) => card.offer.guests === Number.parseInt(filterGuests.value, 10) || filterGuests.value === NOT_SELECTED;
 
 
 const filterMarkers = (cards) => {
   return cards
-    .filter(card => filterByType(card) && filterByRooms(card))
+    .filter(card => filterByType(card) && filterByRooms(card) && filterByGuests(card))
     .slice(0, CARDS_COUNT);
 }
 
