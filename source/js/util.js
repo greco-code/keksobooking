ca488758ab18c8ad825be50f08d53996091c462b
George <naifonovgk@gmail.com>
2021-03-14 22:21:40 +0300

    Fix unused functions removed

--- source/js/util.js
+++ source/js/util.js
@@ -1,38 +1,3 @@
-const getNumberInRange = (min, max) => {
-  return min + Math.random() * (max + 1 - min);
-}
-
-const getRandomIntegerInRange = (min, max) => {
-  const rand = getNumberInRange(min, max);
-
-  if (min >= max || min < 0 || max < 0) {
-    // eslint-disable-next-line no-console
-    console.log('error');
-    return;
-  }
-
-  return (Math.floor(rand));
-}
-
-const getRandomFloatingNumber = (min, max, symbolNumber = 1) => {
-  const rand = getNumberInRange(min, max);
-
-  if (min >= max || min < 0 || max < 0) {
-    // eslint-disable-next-line no-console
-    console.log('error');
-    return;
-  }
-
-  return rand.toFixed(symbolNumber);
-}
-
-const getRandomArrayElement = (arr) => arr[getRandomIntegerInRange(0, arr.length - 1)];
-
-const getNoRepeatElements = (arr) => {
-  return arr.filter(() => Math.random() > 0.5);
-}
-
-
 const declOfNum = (value, words) => {
   value = Math.abs(value) % 100;
   let number = value % 10;
@@ -70,10 +35,6 @@ const debounce = (func, wait, immediate) => {
 
 
 export {
-  getRandomIntegerInRange,
-  getRandomFloatingNumber,
-  getRandomArrayElement,
-  getNoRepeatElements,
   declOfNum,
   isEscEvent,
   debounce
