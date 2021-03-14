b106acfe9df0dd77c5795362e68ffce5d8a9bc29
George <naifonovgk@gmail.com>
2021-03-14 22:25:01 +0300

    Fix ESling errors fixed

--- source/js/util.js
+++ source/js/util.js
@@ -1,9 +1,19 @@
 const declOfNum = (value, words) => {
   value = Math.abs(value) % 100;
   let number = value % 10;
-  if(value > 10 && value < 20) return words[2];
-  if(number > 1 && number < 5) return words[1];
-  if(number === 1) return words[0];
+
+  if (value > 10 && value < 20) {
+    return words[2]
+  }
+
+  if(number > 1 && number < 5) {
+    return words[1]
+  }
+
+  if(number === 1) {
+    return words[0]
+  }
+
   return words[2];
 }
 
@@ -20,7 +30,9 @@ const debounce = (func, wait, immediate) => {
 
     const later = function() {
       timeout = null;
-      if (!immediate) func.apply(context, args);
+      if (!immediate) {
+        func.apply(context, args)
+      }
     };
 
     const callNow = immediate && !timeout;
